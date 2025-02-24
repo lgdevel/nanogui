81df577cf27f32430269be9b0f74219b2e09d5d1
lgdevel <32267381+lgdevel@users.noreply.github.com>
2025-02-24 13:06:00 +0100

    -

--- include/nanogui/imageview_areaselect.h
+++ include/nanogui/imageview_areaselect.h
@@ -6,11 +6,18 @@
 NAMESPACE_BEGIN(nanogui)
 
 class NANOGUI_EXPORT ImageViewAreaSelect : public ImageView {
-
+public:
     using ImageView::ImageView;
 
     // Widget implementation
     virtual bool mouse_drag_event(const Vector2i &p, const Vector2i &rel, int button, int modifiers) override;
+    virtual bool mouse_button_event(const Vector2i &p, int button, bool down, int modifiers) override;
+    virtual bool mouse_motion_event(const Vector2i &p, const Vector2i &rel, int button, int modifiers) override;
+
+private:
+    Vector2i m_first_point;
+    Vector2i m_last_point;
+    bool m_select_in_progress{false};
 };
 
 NAMESPACE_END(nanogui)
--- src/imageview_areaselect.cpp
+++ src/imageview_areaselect.cpp
@@ -19,10 +19,40 @@ bool ImageViewAreaSelect::mouse_drag_event(const Vector2i & p, const Vector2i &r
     if( modifiers != GLFW_MOD_CONTROL ){
         m_offset += rel * screen()->pixel_ratio();
     } else {
-        std::cout << "mouse_drag_event:" << std::endl;
-        std::cout << "\tp: " << p << "; " << "rel: " << rel << std::endl;
-        std::cout << "\tbutton: " << button << "; " << "modifiers: " << modifiers << std::endl;
+        // std::cout << "mouse_drag_event:" << std::endl;
+        // std::cout << "\tp: " << p << "; " << "rel: " << rel << std::endl;
+        // std::cout << "\tbutton: " << button << "; " << "modifiers: " << modifiers << std::endl;
     }
+    return true;
+}
+
+bool ImageViewAreaSelect::mouse_button_event(const Vector2i &p, int button, bool down, int modifiers)
+{
+    if( modifiers == GLFW_MOD_CONTROL && button == GLFW_MOUSE_BUTTON_1 ){
+        if( down ){
+            m_select_in_progress = true;
+            m_first_point = p;
+        } else {
+            m_select_in_progress = false;
+            m_last_point = p;
+            std::cout << "Selected area first: " << m_first_point << "; last: " << m_last_point << std::endl;
+        }
+
+        // std::cout << "mouse_button_event:" << std::endl;
+        // std::cout << "\tp: " << p << "; " << std::endl;
+        // std::cout << "\tbutton: " << button << " " << (down ? "DOWN" : "UP") << std::endl;
+        // std::cout << "\tmodifiers: " << modifiers << std::endl;
+    }
+
+    return true;
+}
+
+bool ImageViewAreaSelect::mouse_motion_event(const Vector2i &p, const Vector2i &rel, int button, int modifiers)
+{
+    if( modifiers == GLFW_MOD_CONTROL ){
+
+    }
+
     return true;
 }
 
